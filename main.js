2d5acfbe5149d2c146d7fe573b2676d86096fe21
artab290 <artab290@gmail.com>
2022-02-11 16:38:10 +0800

    1

--- main.js
+++ main.js
@@ -18,3 +18,20 @@ function signOut() {
     console.log('User signed out.');
   });
 }
+function getGProfile(){
+  var auth2 = gapi.auth2.getAuthInstance();
+  var profile = auth2.currentUser.get().getBasicProfile();
+  console.log(profile.getName());
+  console.log(profile.getEmail());
+}
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
